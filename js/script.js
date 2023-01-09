9c12e6dac89103cde0038bdb9f8f2ea6d503930f
Geletsky <63535039+geletsky-dev@users.noreply.github.com>
2023-01-10 01:07:01 +0200

    Layout of header and section digital

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Меню для мобилбных устройств
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+const menuFooter = document.querySelector(".menu__footer");
+iconMenu.addEventListener("click", function (e) {
+  document.body.classList.toggle("_lock");
+  iconMenu.classList.toggle("_active");
+  menuBody.classList.toggle("_active");
+  menuFooter.classList.toggle("_active");
+});
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+// Проверка темы на уровне системы
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  btnDarkMode.classList.add("_active");
+  document.body.classList.add("dark");
+}
+
+// Проверка темы в local storage
+if (localStorage.getItem("darkMode") == "dark") {
+  btnDarkMode.classList.add("_active");
+  document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") == "light") {
+  btnDarkMode.classList.remove("_active");
+  document.body.classList.remove("dark");
+}
+// Смена темы на сайта
+btnDarkMode.addEventListener("click", function (e) {
+  btnDarkMode.classList.toggle("_active");
+  const isDark = document.body.classList.toggle("dark");
+
+  if (isDark) {
+    localStorage.setItem("darkMode", "dark");
+  } else {
+    localStorage.setItem("darkMode", "light");
+  }
+});
