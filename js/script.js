05ff82cef70b974bd0009781d1ff4dc1ececce56
Geletsky <63535039+geletsky-dev@users.noreply.github.com>
2023-01-27 11:58:01 +0200

    Making small edits
    - Bug fix in the first fullscreen block (not displayed correctly on tablets and phones)
    - Adding clickability to the works and about sections of the blocks
    - Work on validation in the form
    - Adding a smooth transition to sections and fixing the header

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Меню для мобилбных устройств
+//! Меню для мобилбных устройств для header
 const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
 const menuFooter = document.querySelector(".menu__footer");
@@ -8,6 +8,8 @@ iconMenu.addEventListener("click", function (e) {
   menuBody.classList.toggle("_active");
   menuFooter.classList.toggle("_active");
 });
+
+//! Темная тема для сайта
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
 // Проверка темы на уровне системы
@@ -18,7 +20,6 @@ if (
   btnDarkMode.classList.add("_active");
   document.body.classList.add("dark");
 }
-
 // Проверка темы в local storage
 if (localStorage.getItem("darkMode") == "dark") {
   btnDarkMode.classList.add("_active");
@@ -39,16 +40,81 @@ btnDarkMode.addEventListener("click", function (e) {
   }
 });
 
+//! Слайдер для секции feedbacks
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "horizontal",
 
   spaceBetween: 50,
 
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
 
   navigation: {
     nextEl: ".feedbacks-button-next",
     prevEl: ".feedbacks-button-prev",
   },
 });
+
+//! Валидация формы для секции connection
+const formConnection = document.querySelector(".connection__form");
+const inputsConnection = document.querySelectorAll(".connection__form-input");
+const checkboxConnection = document.querySelector("input[type='checkbox']");
+
+// Ставим событие на отправку формы
+formConnection.addEventListener("submit", function (event) {
+  // Проверяем если пользователь поставил флажок на checkbox
+  if (!checkboxConnection.checked) {
+    event.preventDefault();
+    alert("Пожалуйста, установите флажок перед отправкой формы.");
+  }
+});
+
+// Проходимся по каждому input
+for (let i = 0; i < inputsConnection.length; i++) {
+  // Берем текущий input
+  const input = inputsConnection[i];
+
+  // Берем следущий элемент полсе input
+  const label = input.nextElementSibling;
+
+  // Ставим событие focus на текущий input
+  input.addEventListener("focus", function () {
+    label.style.display = "none";
+  });
+
+  // Ставим событие blur на текущий input
+  input.addEventListener("blur", function () {
+    if (!input.value) {
+      label.style.display = "block";
+    }
+  });
+}
+
+//! Плавный переход к секциям
+const sectionLinks = document.querySelectorAll(".section-link");
+for (let i = 0; i < sectionLinks.length; i++) {
+  sectionLinks[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    let targetId = this.getAttribute("href");
+    let target = document.querySelector(targetId);
+    let top = target.offsetTop;
+    window.scrollTo({
+      top: top,
+      behavior: "smooth",
+    });
+    document.body.classList.remove("_lock");
+    iconMenu.classList.remove("_active");
+    menuBody.classList.remove("_active");
+    menuFooter.classList.remove("_active");
+  });
+}
+
+//! Фиксация шапки сверху
+window.onscroll = function showHeader() {
+  const header = document.querySelector(".header");
+
+  if (window.pageYOffset > 200) {
+    header.classList.add("_fixed");
+  } else {
+    header.classList.remove("_fixed");
+  }
+};
