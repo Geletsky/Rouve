8e2e51c02d792b966008e222418c086251893cf7
Geletsky <nsurcov98@gmail.com>
2023-05-24 23:08:42 +0300

    Improved navigation
    - fixing the header
    - smooth transition to sections
    - identification of the active section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
+window.onscroll = () => {
+	const header = document.querySelector('.header');
+
+	if (window.pageYOffset > 200) {
+		header.classList.add('_fixed');
+	} else {
+		header.classList.remove('_fixed');
+	}
+}
+
 function menuBurger() {
 	const menuButton = document.querySelector('.menu__icon');
 	const menuBody = document.querySelector('.menu__body');
 	const menuActions = document.querySelector('.menu__actions');
 
-	menuButton.addEventListener('click', () => {
-		document.body.classList.toggle("_lock");
+	function toggleMenu() {
+		document.body.classList.toggle('_lock');
 		menuButton.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
 		menuActions.classList.toggle('_active');
-	})
+	}
+
+	menuButton.addEventListener('click', toggleMenu);
+
+	window.addEventListener('scroll', () => {
+		if (menuBody.classList.contains('_active')) {
+			toggleMenu();
+		}
+	});
 }
 
 menuBurger();
@@ -34,8 +52,8 @@ function darkMode() {
 		document.body.classList.remove("dark");
 	}
 
-	// Смена темы на сайта
-	btnDarkMode.addEventListener("click", function (e) {
+	btnDarkMode.addEventListener("click", function (event) {
+		event.preventDefault();
 		btnDarkMode.classList.toggle("_active");
 		const isDark = document.body.classList.toggle("dark");
 
@@ -118,3 +136,54 @@ function tabs() {
 }
 
 tabs();
+
+function activeSection() {
+	if (document.querySelectorAll(".section-target").length > 0) {
+		const sections = document.querySelectorAll('.section-target');
+		const sectionsLinks = document.querySelectorAll('.section-link');
+		window.addEventListener("scroll", () => {
+
+			currentIndex = 0;
+			sections.forEach((section, index) => {
+				if (section.offsetTop -60 <= window.scrollY) {
+					currentIndex = index;
+				}
+			});
+
+			const currentSectionId = sections[currentIndex].getAttribute('id');
+
+			sectionsLinks.forEach((link) => {
+				const linkId = link.getAttribute('href').replace('#', '');
+				if (linkId == currentSectionId) {
+					link.classList.add('_active');
+				} else {
+					link.classList.remove('_active');
+				}
+			});
+		});
+	}
+}
+
+activeSection();
+
+function scrollToSections() {
+	document.querySelectorAll('.section-link').forEach(link => {
+		link.addEventListener('click', function (event) {
+			event.preventDefault();
+
+			const href = this.getAttribute('href').substring(1);
+			const section = document.getElementById(href);
+
+			const topOffset = (window.innerWidth <= 480) ? 60 : 0;
+			const sectionPosition = section.getBoundingClientRect().top;
+			const offsetPosition = sectionPosition - topOffset;
+
+			window.scrollBy({
+				top: offsetPosition,
+				behavior: 'smooth'
+			})
+		})
+	})
+}
+
+scrollToSections();
